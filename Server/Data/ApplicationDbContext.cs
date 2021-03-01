8afb0b2f20bd391a84b1a56f95517defa67e997e
Wayan Eka <wayan.eka@volex.com>
2021-03-01 12:59:08 +0700

    Add project files.

---
+++ Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using BlazorWASM_WebAPi.Server.Models;
+using BlazorWASM_WebAPi.Shared;
+using IdentityServer4.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorWASM_WebAPi.Server.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+
+        public DbSet<RecordDetails> RecordDetails { get; set; }
+
+    }
+}
